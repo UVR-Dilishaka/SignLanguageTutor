54e4adc277fe47afd67af2a4ec418a6ce4a846bb
ruwantha <uvrdilishaka@gmail.com>
2025-02-07 22:51:46 +0530

    signup and login logic moved to auth.py

---
+++ app/mvp/backend/auth.py
@@ -0,0 +1,77 @@
+from flask_restx import Namespace, Resource,fields
+from flask import request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from model import User
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+
+auth_ns = Namespace('auth', description='Authentication operations')
+
+signup_model = auth_ns.model(
+    'Signup', {
+        'username': fields.String(required=True),
+        'email': fields.String(required=True),
+        'password': fields.String(required=True),
+        'isteacher': fields.Boolean(required=True)
+    }
+)
+
+
+login_model = auth_ns.model(
+    'Login', {
+        'username': fields.String(required=True),
+        'password': fields.String(required=True)
+    }
+)
+
+
+#jwt authentication
+@auth_ns.route('/signup')
+class Signup(Resource):
+
+    
+    @auth_ns.expect(signup_model)
+    def post(self):
+        data = request.get_json()
+
+        username = data.get('username')
+        db_user = User.query.filter_by(username=username).first()
+
+        if db_user is not None:
+            return jsonify({"message":f"User with username {username} already exists"})
+
+        new_user = User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=generate_password_hash(data.get('password')),
+            isteacher= bool(data.get('isteacher'))
+        )
+
+        new_user.save()
+        return jsonify({"message": f"User with username {username} successfully created"})
+
+
+
+
+
+@auth_ns.route('/login')
+class Login(Resource):
+
+    @auth_ns.expect(login_model)
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        
+        
+        db_user = User.query.filter_by(username=username).first()
+
+        if db_user is None:
+            return jsonify({"message":f"User with username {username} does not exist"})
+        
+        if check_password_hash(db_user.password, password):
+            acess_token = create_access_token(identity=db_user.username)
+            refresh_token = create_refresh_token(identity=db_user.username)
+            return jsonify({"acess_token":acess_token,"refresh_token":refresh_token})
+
+
